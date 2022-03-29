e9b6820077e5168edd58fe016b915edfc2c9c99c
JU MYUNGHOON <myunghoonju@gmail.com>
2022-03-30 01:18:31 +0900

    add batch schema

---
+++ src/main/java/com/practice/batch/config/DBJobConfiguration.java
@@ -0,0 +1,55 @@
+package com.practice.batch.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class DBJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(stepOne())
+                .next(stepTwo())
+                .build();
+    }
+
+    @Bean
+    public Step stepOne() {
+        return stepBuilderFactory.get("stepOne")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("stepOne was executed");
+                        return RepeatStatus.FINISHED; // same as null
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step stepTwo() {
+        return stepBuilderFactory.get("stepTwo")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("stepTwo was executed");
+                        return RepeatStatus.FINISHED; // same as null
+                    }
+                })
+                .build();
+    }
+}
