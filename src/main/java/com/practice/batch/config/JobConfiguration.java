784fcf4b7ac79da188527b2fc42995b8ecefd917
JU MYUNGHOON <myunghoonju@gmail.com>
2022-04-03 16:30:19 +0900

    add custom task

--- src/main/java/com/practice/batch/config/JobConfiguration.java
+++ src/main/java/com/practice/batch/config/JobConfiguration.java
@@ -1,20 +1,15 @@
 package com.practice.batch.config;
 
+import com.practice.batch.tasks.CustomTasklet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -35,21 +30,7 @@ public class JobConfiguration {
     @Bean
     public Step jobStepOne() {
         return stepBuilderFactory.get("jobStepOne")
-                .tasklet((StepContribution contribution, ChunkContext chunkContext) ->{
-                    // JobParameters :: preferred
-                    JobParameters params = contribution.getStepExecution().getJobExecution().getJobParameters();
-                    Map<String, JobParameter> paramMap = params.getParameters();
-                    JobParameter name = paramMap.get("name");
-                    JobParameter seq = paramMap.get("seq");
-                    JobParameter date = paramMap.get("date");
-                    JobParameter age = paramMap.get("age");
-
-                    // Map :: can't use them, read only
-                    Map<String, Object> paramFromChunkContext = chunkContext.getStepContext().getJobParameters();
-
-                    log.info("jobStepOne executed");
-                        return RepeatStatus.FINISHED;
-                })
+                .tasklet(new CustomTasklet())
                 .build();
     }
 
---
+++ src/main/java/com/practice/batch/tasks/CustomTasklet.java
@@ -0,0 +1,32 @@
+package com.practice.batch.tasks;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import java.util.Map;
+
+@Slf4j
+public class CustomTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        // JobParameters :: preferred
+        JobParameters params = contribution.getStepExecution().getJobExecution().getJobParameters();
+        Map<String, JobParameter> paramMap = params.getParameters();
+        JobParameter name = paramMap.get("name");
+        JobParameter seq = paramMap.get("seq");
+        JobParameter date = paramMap.get("date");
+        JobParameter age = paramMap.get("age");
+
+        // Map :: can't use them, read only
+        Map<String, Object> paramFromChunkContext = chunkContext.getStepContext().getJobParameters();
+
+        log.info("jobStepOne executed");
+        return RepeatStatus.FINISHED;
+    }
+}
